f7b2efd9104b2d8cc7eb5507800946618f047110
SamFisher0208 <sammee1@gmail.com>
2024-02-14 11:52:01 -0600

    added get count function

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 def main():
     path_to_file = "books/frankenstein.txt"
     text = get_text(path_to_file)
-    print(text)
+    count = get_count(text)
+    print(count)
 
-    
 def get_text(path):
     with open(path) as f:
-        return f.read() 
+        return f.read()
 
+def get_count(text):
+    list_of_words = text.split()
+    return len(list_of_words)
+
+def get_count_letters(text):
+    return
 
 if __name__ == '__main__':
     main()
